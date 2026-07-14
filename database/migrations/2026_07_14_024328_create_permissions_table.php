4f790d1c25f460a39f12f48abc2461013b809ee4
yoshiidesuu <yoshiidesuu@gmail.com>
2026-07-14 10:44:56 +0800

    Fix CI: add larastan, phpstan config; fix code style

--- database/migrations/2026_07_14_024328_create_permissions_table.php
+++ database/migrations/2026_07_14_024328_create_permissions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
-            
+
             $table->unique(['role_id', 'name']);
         });
     }
